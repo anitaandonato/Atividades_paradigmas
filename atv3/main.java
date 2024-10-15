b23d45ce119f20b050ccfe7be18fe131db4f64c6
Anita <130478177+anitaandonato@users.noreply.github.com>
2024-10-14 21:49:28 -0300

    Create main.java

---
+++ atv3/main.java
@@ -0,0 +1,25 @@
+class ContaBancaria {
+    private double saldo;
+    private String titular;
+
+    ContaBancaria(String titular, double saldoInicial) {
+        this.titular = titular;
+        this.saldo = saldoInicial;
+    }
+
+    public void depositar(double valor) {
+        saldo += valor;
+    }
+
+    public void sacar(double valor) {
+        if (valor <= saldo) {
+            saldo -= valor;
+        } else {
+            System.out.println("Saldo insuficiente.");
+        }
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+}
